aa318e3d110238b2bab023b02e1a205cb5afdb9b
Alexander Dietrich <if16b063@technikum-wien.at>
2017-10-13 17:47:58 +0200

    started send_class

--- MyServer/SendMessage.cpp
+++ MyServer/SendMessage.cpp
@@ -4,17 +4,35 @@
 
 #include <netinet/in.h>
 #include <queue>
+#include <string>
 #include "SendMessage.h"
 
-SendMessage::SendMessage(string rawMessage) : ServerOperation(rawMessage) {
+SendMessage::SendMessage() : ServerOperation() {
 
-}
-
-bool SendMessage::parse(string Message) {
-    return false;
 }
 
 string SendMessage::execute() {
+
     return std::__cxx11::string();
+}
+
+bool SendMessage::fillMe(string message) {
+    if(message != "."){
+        switch(index){
+            case 1:
+                sender = message;
+                return true;
+            case 2:
+                receiver = message;
+                return true;
+            case 3:
+                subject = message;
+                return true;
+            default:
+                message_final += message;
+                return true;
+        }
+    }
+    return false;
 }
 
