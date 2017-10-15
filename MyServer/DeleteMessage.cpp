8d995521e353efff7191d172cec4f8c0b759ef30
Alexander Dietrich <if16b063@technikum-wien.at>
2017-10-15 20:42:08 +0200

    delete message - implementierung gestartet

--- MyServer/DeleteMessage.cpp
+++ MyServer/DeleteMessage.cpp
@@ -2,5 +2,54 @@
 // Created by alexander on 12.10.17.
 //
 
+#include <dirent.h>
 #include "DeleteMessage.h"
 
+DeleteMessage::DeleteMessage() : ServerOperation() {
+    parameter_count = 0;
+    chosen_message = 0;
+    username = "";
+}
+
+bool DeleteMessage::fillMe(string message) {
+    //fill the needed parameter
+
+    switch(parameter_count){
+        case 0:
+            if(message.length() <= 9 && message.length() > 0){
+                parameter_count ++;
+                username = message;
+                username.pop_back();
+                statusMessage = "valid input!";
+                return true;
+            }else {
+                statusMessage = "invalid username - Max 8 Characters!";
+                return false;
+            }
+        case 1:
+            if(!message.empty() && isdigit(message[0]) && message[0] != '0'){
+                chosen_message = stoi(message);
+                statusMessage = "valid parameter - not executed yet!";
+                return true;
+            }else{
+                statusMessage = "Invalid Message-Number - Must be a number bigger than 0!";
+                return false;
+            }
+        default:
+            statusMessage = "Fatal Error! Parameter is not set! Connection closed";
+            return false;
+    }
+}
+
+string DeleteMessage::execute() {
+    string result = "";
+    string dir = MESSAGEDIR + "/" + username + "/";
+
+    const char * dir_char = dir.c_str();
+    int delete_result = remove(dir_char);
+
+    result = to_string(delete_result);
+    statusMessage = "OK\n";
+
+    return result;
+}
